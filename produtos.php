efd9a973d9d83ce8c4c8bcfb8fb3c56cb01b6c35
Rafael Jaques <rafael@phpit.com.br>
2023-02-24 11:02:16 -0300

    Testes com twig

--- produtos.php
+++ produtos.php
@@ -8,6 +8,27 @@
 
     $template = $twig->load('produtos.html');
 
+    $produtos = [
+        [
+            'nome' => 'Chinelo',
+            'preco' => 30,
+        ],
+        [
+            'nome' => 'Camiseta',
+            'preco' => 50,
+        ],
+        [
+            'nome' => 'Boné',
+            'preco' => 39.9,
+        ],
+        [
+            'nome' => 'Automóvel simples',
+            'preco' => 350000,
+        ],
+    ];
+
     echo $template->render([
-        'titulo' => 'Produtos'
+        'titulo' => 'Produtos',
+        // 'produtos' => $produtos,
+        'produtos' => null,
     ]);
