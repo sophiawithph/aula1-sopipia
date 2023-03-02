93dd935f60045e3c97af0a5e4d28096247645e4d
Rafael Jaques <rafael@phpit.com.br>
2023-03-02 10:00:22 -0300

    Criação de página de login

---
+++ boasvindas.php
@@ -0,0 +1,5 @@
+<?php
+    # /boasvindas.php
+    require('twig_carregar.php');
+
+    echo $twig->render('boasvindas.html');
---
+++ login_verifica.php
@@ -0,0 +1,14 @@
+<?php
+    # login_verifica.php
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+
+    if ($user == 'rafael' && $pass == '123') {
+        // Login feito com sucesso
+        header('location:boasvindas.php');
+        die;
+    } else {
+        // Falha no login
+        header('location:login.php?erro=1');
+        die;
+    }
